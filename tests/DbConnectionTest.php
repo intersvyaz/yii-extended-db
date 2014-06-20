cae51a6a3ad85e2ce68533470866d08aed993c3e
rustik <rustik@intersvyaz.net>
2014-06-20 11:09:00 +0600

    Add detachEventHandler in test

--- tests/DbConnectionTest.php
+++ tests/DbConnectionTest.php
@@ -7,12 +7,20 @@ class DbConnectionTest extends PHPUnit_Framework_TestCase
 	public function testOpen()
 	{
 		Yii::app()->db->setActive(false);
-		Yii::app()->db->onAfterOpen = function () {
-			$this->afterOpened = true;
-		};
-
+		Yii::app()->db->attachEventHandler('onAfterOpen', [$this, 'eventHandler']);
 		Yii::app()->db->setActive(true);
 		$this->assertTrue($this->afterOpened);
+
+		$this->afterOpened = false;
+		Yii::app()->db->detachEventHandler('onAfterOpen', [$this, 'eventHandler']);
+		Yii::app()->db->setActive(false);
+		Yii::app()->db->setActive(true);
+		$this->assertFalse($this->afterOpened);
+	}
+
+	public function eventHandler()
+	{
+		$this->afterOpened = true;
 	}
 
 	public function testCreateCommand()
